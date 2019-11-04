c1d6ca60b318f6d636fcf44b25c6d27d90d12b71
Mariola Pasko <paskomariola@gmail.com>
2019-11-04 11:35:59 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,3 +2,6 @@ def reverse_each_word(string)
 string.split().each {|word| word.reverse!}.join(" ")
 end 
 
+def reverse_each_word(string)
+string.split().collect {|word| word.reverse}.join(" ")
+end
